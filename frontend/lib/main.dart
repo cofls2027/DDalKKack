e5e4c7708836b073119e4b8c0795d1e348423ff0
원 의재 <wonuijae@gmail.com>
2026-05-10 16:38:17 +0900

    front삭제

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/gallery_screen.dart';
-import 'services/api_service.dart';
 
-final apiService = ApiService();
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await apiService.init();  // 토큰 자동 발급
+void main() {
   runApp(const MyApp());
 }
 
@@ -21,7 +15,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const GalleryScreen(),
+      home: const Scaffold(
+        body: Center(child: Text('DDalKKack')),
+      ),
     );
   }
 }
